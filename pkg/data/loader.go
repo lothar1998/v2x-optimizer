5b418a3e284ecfa50c5a54932772c4f0480abfc8
Piotr Kuglin <33781380+lothar1998@users.noreply.github.com>
2021-07-25 10:52:01 +0200

    serializer (#3)

--- pkg/data/loader.go
+++ pkg/data/loader.go
@@ -5,8 +5,7 @@ import (
 	"os"
 )
 
-// Load allows for conveniently loading
-// data in json format to Data structure
+// Load allows for conveniently loading data in json format to Data structure
 func Load(filepath string) (*Data, error) {
 	file, err := os.Open(filepath)
 	if err != nil {
@@ -23,4 +22,18 @@ func Load(filepath string) (*Data, error) {
 	}
 
 	return &data, nil
+}
+
+// Export facilitates exporting Data to json file in given filepath.
+// Path should exist before exporting.
+func Export(filepath string, data *Data) error {
+	file, err := os.OpenFile(filepath, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(data)
+	return err
 }
