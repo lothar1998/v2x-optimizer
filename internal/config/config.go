c9a9c701be70039b77494ec996f6a4ffcd0d8975
Piotr Kuglin <33781380+lothar1998@users.noreply.github.com>
2021-09-07 19:31:35 +0200

    fix

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,7 +3,7 @@ package config
 import "github.com/lothar1998/v2x-optimizer/pkg/optimizer"
 
 // CPLEXOptimizerName is a name of cplex optimizer. It needs to be defined here
-// because CPLEX doesn't have optimizer.Optimizer implementation
+// because CPLEX doesn't have optimizer.Optimizer implementation.
 const CPLEXOptimizerName = "cplex"
 
 // RegisteredOptimizers is a list of all possible optimizers.
