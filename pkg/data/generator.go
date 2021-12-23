c2a88641daa0da0baf5097fb75e2d0f3a0539b9b
Piotr Kuglin <33781380+lothar1998@users.noreply.github.com>
2021-12-23 12:08:53 +0100

    Add exp and norm generators (#32)

--- pkg/data/generator.go
+++ pkg/data/generator.go
@@ -1,22 +1,70 @@
 package data
 
 import (
+	"math"
 	"math/rand"
 	"time"
 )
 
-// RLimit defines upper bound for R value
-var RLimit = 100
+type generateFunc = func(limit int) int
 
-// Generate facilitates generating random data with Data.R values constraint to given RLimit.
-// Data.MRB values are bounded by the computed limit to provide data that create a solvable problem.
-func Generate(v, n int) *Data {
+var (
+	RLimit = 100
+	gen    = rand.New(rand.NewSource(time.Now().UnixNano()))
+)
+
+func GenerateUniform(v, n int) *Data {
+	f := func(limit int) int {
+		return gen.Intn(limit) + 1
+	}
+
+	return generate(v, n, f)
+}
+
+func GenerateNormal(v, n int) *Data {
+	f := func(limit int) int {
+		mean := float64(limit) / 2
+		stdDev := float64(limit) / 3
+
+		for {
+			normValue := gen.NormFloat64()*stdDev + mean
+			flooredValue := math.Ceil(normValue)
+			intValue := int(flooredValue)
+
+			if intValue > 0 && intValue <= limit {
+				return intValue
+			}
+		}
+	}
+
+	return generate(v, n, f)
+}
+
+func GenerateExponential(v, n int) *Data {
+	f := func(limit int) int {
+		mean := float64(limit) / 4
+		for {
+			gen.ExpFloat64()
+			expValue := gen.ExpFloat64() * mean
+			flooredValue := math.Ceil(expValue)
+			intValue := int(flooredValue)
+
+			if intValue > 0 && intValue <= limit {
+				return intValue
+			}
+		}
+	}
+
+	return generate(v, n, f)
+}
+
+func generate(v, n int, generate generateFunc) *Data {
 	r := make([][]int, v)
 
 	for i := 0; i < v; i++ {
 		r[i] = make([]int, n)
 		for j := 0; j < n; j++ {
-			r[i][j] = rand.Intn(RLimit) + 1
+			r[i][j] = generate(RLimit)
 		}
 	}
 
@@ -24,12 +72,8 @@ func Generate(v, n int) *Data {
 	mrbLimit := RLimit * v / n * 2
 
 	for i := 0; i < n; i++ {
-		mrb[i] = rand.Intn(mrbLimit) + 1
+		mrb[i] = gen.Intn(mrbLimit)
 	}
 
 	return &Data{R: r, MRB: mrb}
-}
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
 }
